a26c0bd6e58ef43ed0e70cd1d81b600baf5c8eba
bamdad <bamdad.kho@gmail.com>
2026-07-31 13:59:28 -0700

    docs: finished design doc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,17 @@
 import Fastify from 'fastify';
 
 const app = Fastify();
-const port = 1000;
+const port = 3030;
+
+let numPressed = 0;
 
 app.post('/button', async (_req, reply) => {
-    console.log('button pressed');
+    numPressed++;
+    console.log(`button pressed ${numPressed} time${numPressed>1 ? 's' : ''}`);
     reply.code(200).send();
 });
 
-app.listen({ port }, (err) => {
+app.listen({ port, host: '0.0.0.0' }, (err) => {
     if (err) {
         console.error('server failed to start:', err);
         process.exitCode = 1;
